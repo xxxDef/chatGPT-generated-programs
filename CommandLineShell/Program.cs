2a9abea6c4190ec008cbfd1fb0ad7fe9df2d4f3a
Sergey Lapsky <sergey.lapsky@hotmail.com>
2023-05-03 17:44:24 +0300

    request: write C# program which allow to call public method of any class defined in this program. Class name and method name to call should be provided in comand line.

---
+++ CommandLineShell/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Usage: Program <ClassName> <MethodName>");
+            return;
+        }
+
+        string className = args[0];
+        string methodName = args[1];
+
+        Type type = Type.GetType(className);
+        if (type == null)
+        {
+            Console.WriteLine($"Class {className} not found.");
+            return;
+        }
+
+        object instance = Activator.CreateInstance(type);
+        if (instance == null)
+        {
+            Console.WriteLine($"Failed to create instance of {className}.");
+            return;
+        }
+
+        var methodInfo = type.GetMethod(methodName);
+        if (methodInfo == null)
+        {
+            Console.WriteLine($"Method {methodName} not found in class {className}.");
+            return;
+        }
+
+        methodInfo.Invoke(instance, null);
+    }
+}
