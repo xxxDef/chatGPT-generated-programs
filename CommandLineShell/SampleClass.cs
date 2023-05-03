cbadd0ffe9c928c8173a795174903c1e27f1c830
Sergey Lapsky <sergey.lapsky@hotmail.com>
2023-05-03 18:06:48 +0300

    add sample class which can be called in program with some sample methods - one without parameters, second - with simgle string parameter and last - with string, int and date parameter

---
+++ CommandLineShell/SampleClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class SampleClass
+{
+    public void MethodWithoutParameters()
+    {
+        Console.WriteLine("MethodWithoutParameters called");
+    }
+
+    public void MethodWithOneParameter(string str)
+    {
+        Console.WriteLine($"MethodWithOneParameter called with parameter: {str}");
+    }
+
+    public void MethodWithMultipleParameters(string str, int num, DateTime date)
+    {
+        Console.WriteLine($"MethodWithMultipleParameters called with parameters: {str}, {num}, {date}");
+    }
+}
